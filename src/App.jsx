9e2404c5f51f2ea1ee5bd6a7c5bdec4c2706073b
Davronn <davronmahamadjonov8@gmai.com>
2024-04-08 15:59:52 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Students from "./pages/Students";
 import Profile from "./pages/Profile";
+import Loginteat from "./pages/Logintest";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
+          {/* <Route path="/" element={<Login />} /> */}
           <Route path="/" element={<Login />} />
           <Route path="/students" element={<Students />} />
           <Route path="/profile" element={<Profile />} />
